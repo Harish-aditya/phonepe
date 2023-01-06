09ef5cd8cccadba904107db72946992e337c85df
Harish Aditya <harishaditya1308@gmail.com>
2023-01-06 08:21:24 +0530

    comit

---
+++ trail.py
@@ -0,0 +1,51 @@
+# #list
+# list = [1,2,3,4,5]
+# print(f"Orginal List: {list}")
+# # Slicing
+# list1 = list[:2]
+# print(f"Orginal List: {list1}")
+# # Concatenating
+# list2 = [6,7,8,9,10]
+# list = list + list2
+# print(f"Concatenated list {list}")
+
+
+# # Tuples
+# tuple1 = ("a", "b" ,"c")
+# tuple2 = (1, 2, 3)
+
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+# # Removing items
+# thistuple = ("apple", "banana", "cherry")
+# print(f"Before remove {thistuple}")
+# y = list(thistuple)
+# y.remove("apple")
+# thistuple = tuple(y)
+# print(f"After remove {thistuple}")
+
+
+# f = "Hi, How are you?"
+
+# #slicing
+# f_slice = f[:3]
+# print(f_slice)
+
+# # Formatting
+# name = 'XYZ'
+# print(f"Hi {name}, How are you?")
+
+
+# Dictionery
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+
+# getting keywords from dictionery
+x = thisdict.keys()
+print(x)
